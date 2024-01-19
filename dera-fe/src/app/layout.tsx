e4346de3083a5b1a0d0b91db42b72a0369f731fa
yongwee <yongwee1@gmail.com>
2024-01-19 18:53:01 +0800

    Add Google Tag Manager support

--- dera-fe/src/app/layout.tsx
+++ dera-fe/src/app/layout.tsx
@@ -8,12 +8,25 @@ import { dark } from '@clerk/themes';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
+import Script from 'next/script';
 import './style.css';
 
 export const metadata: Metadata = {
   title: 'Dera',
   description: 'Data Encoding and Representation Analysis',
 };
+
+const gtagScript = process.env.NEXT_PUBLIC_GTM_ID && (
+  <Script id="google-tag-manager" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+      `}
+  </Script>
+);
 
 export default function RootLayout({
   children,
@@ -30,6 +43,7 @@ export default function RootLayout({
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
           />
+          {gtagScript}
         </head>
         <body>
           <MantineProvider theme={theme} forceColorScheme="dark">
